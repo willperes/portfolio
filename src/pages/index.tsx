3c4cc367760a4dd781a096639971432eefd1e273
willperes <thewidocs@gmail.com>
2024-04-03 15:16:00 -0300

    fix: page content max-width

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,8 +16,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <Hero />
+      <main className={"flex justify-center"}>
+        <div className={"content-max-width"}>
+          <Hero />
+        </div>
       </main>
     </>
   );
