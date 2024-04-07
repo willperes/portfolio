43db3facbdcdaa05a68990a027c79c6492c7dae6
willperes <thewidocs@gmail.com>
2024-04-06 21:42:16 -0300

    fix: webmanifest and missing head information

--- src/components/shared/Head/index.tsx
+++ src/components/shared/Head/index.tsx
@@ -15,7 +15,9 @@ export const Head: React.FC<React.PropsWithChildren<Props>> = ({
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon.ico" sizes="48x48" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      <link rel="manifest" href="/site.webmanifest" />
       {children}
     </NextHead>
   );
