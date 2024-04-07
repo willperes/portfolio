f2e726ef8bd9305567c23befb6ca3ad35e9b0547
willperes <thewidocs@gmail.com>
2024-04-06 22:41:01 -0300

    fix: change title instead of description

--- src/components/shared/Head/index.tsx
+++ src/components/shared/Head/index.tsx
@@ -12,8 +12,8 @@ export const Head: React.FC<React.PropsWithChildren<Props>> = ({
 }) => {
   return (
     <NextHead>
-      <title>{title}</title>
-      <meta name="description" content={`${description} | Willian Peres`} />
+      <title>{`${title} | Willian Peres`}</title>
+      <meta name="description" content={description} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" sizes="48x48" />
       <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
