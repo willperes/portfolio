de58213824666cbf04e2c8f1c2ecfeddefb84b8d
willperes <thewidocs@gmail.com>
2024-04-20 23:01:23 -0300

    fix: change about me text

--- src/components/pages/Home/AboutMe/index.tsx
+++ src/components/pages/Home/AboutMe/index.tsx
@@ -7,13 +7,13 @@ export const AboutMe: React.FC = () => {
       <h1 className={"text-section-title mb-8 sm:mb-12"}>ABOUT ME</h1>
 
       <p className={"text-secondary text-center responsive-text-sm"}>
-        Mobile Engineer/Front-end Developer with more than 2 years of hands-on
-        experience in web and mobile development with ReactJS and React Native,
-        and integration with Firebase and crash/performance monitoring. I have
-        created and published an e-commerce app from scratch with more than 5000
-        users. I also have experience with backend development with NestJS,
-        databases such as PostgreSQL and MongoDB and with the message broker
-        software RabbitMQ.
+        Mobile and Frontend Developer with more than 2 years of hands-on
+        experience in mobile development using React Native, experienced in
+        Firebase integration and crash/performance monitoring, and web
+        development with ReactJS and NextJS. I have created and published an
+        e-commerce app using React Native from scratch with more than 5000
+        users. I also have experience with backend development using NodeJS
+        along with the framework NestJS.
       </p>
     </section>
   );
